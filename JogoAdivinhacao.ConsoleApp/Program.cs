092b8108c7d196b3f56745f9700db6802d1def16
chayannerech <rechchayanne@gmail.com>
2024-04-02 16:11:50 -0300

    Adicionado histórico.

--- JogoAdivinhacao.ConsoleApp/Program.cs
+++ JogoAdivinhacao.ConsoleApp/Program.cs
@@ -4,26 +4,43 @@
     {
         static void Main(string[] args)
         {
+            string[] historico = new string[100];
+            int j = 0;
+            DateTime thisDay = DateTime.Now;
+            
             while (true)
             {
                 int numAleatorio, numChutado, i, pontos, dificuldade;
-                IniciaVariaveis(out numAleatorio, out i, out pontos, out dificuldade);
+                IniciaVariaveis(out numAleatorio, out i, j, out pontos, out dificuldade, ref historico);
+                
                 numChutado = RecebeTentativas(numAleatorio, ref i, ref pontos, dificuldade);
+                Resultado(numChutado, numAleatorio, ref historico, j, pontos, thisDay);
 
-                if (numChutado == numAleatorio) Console.WriteLine($"Você ganhou! :) Sua pontuação é {pontos} pontos");
-                else Console.WriteLine("Você perdeu :(");
-
-                if (DeveContinuar()) break;
+                if (!DeveContinuar("\nDeseja visualizar o histórico? [S,N]")) MostrarHistorico(historico, j);
+                if (DeveContinuar("\nDeseja repetir? [S,N]")) break;
+                j++;
             }
         }
-        static void IniciaVariaveis(out int numAleatorio, out int i, out int pontos, out int dificuldade)
+        static void IniciaVariaveis(out int numAleatorio, out int i, int j, out int pontos, out int dificuldade, ref string[] historico)
         {
             Console.Clear();
             Random objAleatorio = new Random();
             numAleatorio = objAleatorio.Next(20);
-            i = 0;
-            pontos = 1000;
+            i = 0; pontos = 1000;
+
+            Console.Write("            JOGO DE ADIVINHAÇÃO           \n---------------------------------------------\ntente adivinhar o número secreto entre 0 e 20\n---------------------------------------------\n\nInforme o seu nome: ");
+            historico[j] = Console.ReadLine();
             dificuldade = EscolheDificuldade();
+        }
+        static int EscolheDificuldade()
+        {
+            Console.Clear();
+            Console.WriteLine("            JOGO DE ADIVINHAÇÃO           \n---------------------------------------------\ntente adivinhar o número secreto entre 0 e 20\n---------------------------------------------\n       Escolha o nível de dificuldade:\n\n 1. Fácil\t 2. Médio\t 3. Difícil\n");
+            int dificuldade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\nÓtimo. Você tem {20 - 5 * dificuldade} tentativas\n");
+
+            return dificuldade;
         }
         static int RecebeTentativas(int numAleatorio, ref int i, ref int pontos, int dificuldade)
         {
@@ -41,26 +58,32 @@
 
             return numChutado;
         }
+        static void Resultado(int numChutado, int numAleatorio, ref string[] historico, int j, int pontos, DateTime thisDay)
+        {
+            if (numChutado == numAleatorio) Console.WriteLine($"\n---------------------------------------------\nVocê ganhou!! :) Sua pontuação é {pontos} pontos\n---------------------------------------------");
+            else Console.WriteLine("Você perdeu :(");
+
+            historico[j] = historico[j] + $" -> {pontos} ({thisDay.ToString("g")})";
+        }
         static void MaiorOUmenor(int numAleatorio, int numChutado)
         {
             if (numChutado > numAleatorio) Console.WriteLine("Seu chute foi maior que o número secreto!\n");
             if (numChutado < numAleatorio) Console.WriteLine("Seu chute foi menor que o número secreto!\n");
         }
-        static int EscolheDificuldade()
+        static bool DeveContinuar(string texto)
         {
-            Console.WriteLine("JOGO DE ADIVINHAÇÃO\nEscolha o nível de dificuldade:\n\n 1. Fácil\t 2. Médio\t 3. Difícil\n");
-            int dificuldade = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine($"\nÓtimo. Você tem {20 - 5 * dificuldade} tentativas\n");
-
-            return dificuldade;
-        }
-        static bool DeveContinuar()
-        {
-            Console.WriteLine("\nDeseja repetir? [S,N]");
+            Console.WriteLine(texto);
             string continuar = Console.ReadLine();
 
             return continuar == "n" || continuar == "N";
+        }
+        static void MostrarHistorico(string[] historico, int j)
+        {
+            Console.WriteLine($"\n\n---------------------------------------------\n                HISTÓRICO");
+
+            for (int k = 0; k <= j; k++) Console.WriteLine(historico[k]);
+
+            Console.WriteLine($"---------------------------------------------\n");
         }
     }
 }
